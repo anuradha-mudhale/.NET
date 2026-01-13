17f8533e504327a2abae34337d9d7613127298d5
anuradha-mudhale <anuradhamudhale314@gmail.com>
2026-01-13 13:09:12 -0800

    files added

---
+++ Arithmetic.cs
@@ -0,0 +1,18 @@
+using System;
+namespace operations
+{
+	class ArithmeticOperations
+
+		{
+		  public static void Main()
+{
+			int a=10;
+			int b=20;
+			Console.WriteLine("Addition is"+(a+b));
+			Console.WriteLine("Substraction is"+(a-b));
+			Console.WriteLine("Multiplication is:"+(a*b));
+			Console.WriteLine("Dvision is:"+(a/b));
+}
+
+		}
+}
