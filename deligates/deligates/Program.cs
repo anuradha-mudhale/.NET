09126d5aa61505132f8f4274681e1b641c5bc6bc
anuradha-mudhale <anuradhamudhale314@gmail.com>
2026-04-26 15:56:20 +0530

    Add files via upload

---
+++ deligates/deligates/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deligates
+{
+    internal class Program
+    {
+        public delegate void mydel();
+        public static void fun1()
+        {
+            Console.WriteLine("f1() called");
+        }
+
+        public static void fun2()
+        {
+            Console.WriteLine("Function called");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();            
+            mydel del = new mydel(fun1);
+            del += fun2;
+            del();
+           
+        }
+    }
+
+
+}
---
+++ deligates/deligates/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("deligates")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("deligates")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("812c0c24-bbbc-434e-8acd-59cc5a16cf60")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
