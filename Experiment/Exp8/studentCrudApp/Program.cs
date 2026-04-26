e81f749e42040376d609ae4af48ab6449b510d14
anuradha-mudhale <anuradhamudhale314@gmail.com>
2026-04-26 15:31:50 +0530

    Exp8 Added

---
+++ Experiment/Exp8/studentCrudApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using StudentCRUDApp.Data;
+using StudentCRUDApp.Models;
+
+class Program
+{
+    static void Main()
+    {
+        using (var context = new AppDbContext())
+        {
+            // CREATE
+            var student = new Student
+            {
+                Name = "Anuradha",
+                Age = 18,
+                Course = "IT"
+            };
+            context.Students.Add(student);
+            context.SaveChanges();
+
+            Console.WriteLine("Student Added!");
+
+            // READ
+            var students = context.Students.ToList();
+            foreach (var s in students)
+            {
+                Console.WriteLine($"{s.Id} {s.Name}");
+            }
+
+            // UPDATE
+            var firstStudent = context.Students.FirstOrDefault();
+            if (firstStudent != null)
+            {
+                firstStudent.Name = "Updated Anuradha";
+                context.SaveChanges();
+                Console.WriteLine("Student Updated!");
+            }
+
+            // DELETE
+            var deleteStudent = context.Students.FirstOrDefault();
+            if (deleteStudent != null)
+            {
+                context.Students.Remove(deleteStudent);
+                context.SaveChanges();
+                Console.WriteLine("Student Deleted!");
+            }
+        }
+    }
+}
